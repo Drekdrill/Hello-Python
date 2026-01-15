70d631831081294fd67b98c777629417ae71ba48
Dulsara Olitha <149897654+Drekdrill@users.noreply.github.com>
2026-01-15 18:44:06 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,1 @@
+print("Hello My name is Dulsara Olitha. I'm an IT Undergraduate from Open University Sri Lanka. This python script is for to demonstrate how to upload a file to a github repository")
